fafa7941507c69a0729e022c7b76660e59cc31ae
zakariya aboudahab <zakariaaboudahab1@zakarias-MacBook-Pro.local>
2026-01-20 15:56:28 +0100

    Implementado menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Git");
+        int opcion = menuPrincipal();
+        System.out.println("Has seleccionado la opción: " + opcion);
     }
+
+    private static int menuPrincipal() {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("1. Estado");
+            System.out.println("2. Comer");
+            System.out.println("3. Jugar");
+            System.out.println("4. Dormir");
+            System.out.println("----------------");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción del menú: ");
+
+            while (!sc.hasNextInt()) {
+                System.out.println("Opción no válida. Introduce un número.");
+                sc.next(); // limpiar entrada incorrecta
+                System.out.print("Seleccione una opción del menú: ");
+            }
+
+            opcion = sc.nextInt();
+
+            if (opcion < 0 || opcion > 4) {
+                System.out.println("Opción no válida. Inténtelo de nuevo.\n");
+            }
+
+        } while (opcion < 0 || opcion > 4);
+
+        return opcion;
+    }
+
 }
